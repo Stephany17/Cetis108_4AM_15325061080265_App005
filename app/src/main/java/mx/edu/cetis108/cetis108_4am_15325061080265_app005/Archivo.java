d1787d09a94148b65c52756b3252caccd1c0b4e8
Stephany <15325061080265@cetis108.edu.mx>
2017-06-07 16:29:30 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/cetis108/cetis108_4am_15325061080265_app005/Archivo.java
@@ -0,0 +1,23 @@
+package mx.edu.cetis108.cetis108_4am_15325061080265_app005;
+
+import java.util.Date;
+
+/**
+ * Created by Stephany on 07/06/2017.
+ */
+
+public class Archivo {
+
+    String usuario;
+    String nombre;
+    String tipo;
+    String ruta;
+    Date fechaCreacion;
+    Date fechaModificacion;
+
+
+    public Archivo()
+    {
+
+    }
+}
